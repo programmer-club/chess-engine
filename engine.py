2775adcf36ec9dbbeb804924f425b05358d7b731
Corrupt10n404 <52146696+Corrupt10n404@users.noreply.github.com>
2019-06-28 22:01:40 -0700

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,9 @@
+import math
+class Engine:
+  def __init__(self):
+    self.best_move = 0
+    self.deepness = 0
+    #Other initiation files
+  def find_best_move(self):
+     #Implement recursion and alpha-beta move analysis
+  #Add more to the chess engine
